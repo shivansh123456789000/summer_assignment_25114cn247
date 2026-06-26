9e00b4d397939eb0cb693bee68333427600a1201
shivansh123456789000 <shivanshsingh6540@gmail.com>
2026-06-26 18:39:58 +0530

    1-4 question

---
+++ day 26/day 26 (3).c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+    int withdraw , deposit, n ,current_balance = 500;
+    printf("-----------atm.  menu  --------  \n");
+    printf("1 for check balance : \n");
+    printf("2 for deposit :\n");
+    printf("3 for withdraw :\n");
+    printf("4 for exit ");
+    printf("enter your choice : \n");
+    scanf("%d",&n);
+    if (n==1){
+        
+        printf("your balance is %d :\n" ,current_balance);
+
+
+    }
+        
+    else if(n==3){
+        printf("enter the amount : ");
+        scanf("%d", &withdraw);
+        if (withdraw<=current_balance){
+            current_balance= current_balance-withdraw;
+
+            printf("amount withdrwan : %d\n" ,withdraw);
+            printf("remaining balance: %d\n", current_balance);
+
+
+        }
+        else {
+            printf("insufficient balance ");
+
+        }
+    }
+    else if (n==2){
+        printf("enter the amount you want to deposit : ");
+        scanf("%d", &deposit);
+        current_balance= current_balance+deposit;
+        printf("amount successfully deposited in your account : %d\n", deposit);
+        printf("current balance now : %d \n" , current_balance);
+    }
+    else {
+        printf("thank you for visiting to our meow moew bank");
+    }
+}
